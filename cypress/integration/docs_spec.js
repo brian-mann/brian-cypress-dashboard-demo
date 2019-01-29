40bb0200c42de3a2245537416ca6f3e67ee79f06
Brian Mann <brian.mann86@gmail.com>
2019-01-29 13:13:59 -0500

    example of cypress

---
+++ cypress/integration/docs_spec.js
@@ -0,0 +1,31 @@
+describe('cypress docs', () => {
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.visit('https://docs.cypress.io')
+    })
+    
+    it('it does not display hamburger icon', () => {
+      cy
+      .get('#mobile-nav-toggle')
+      .should('not.be.visible')
+    })
+  })
+
+  context.only('mobile', () => {
+    beforeEach(() => {
+      cy.visit('https://docs.cypress.io')
+    })
+
+    ;['iphone-3', 'iphone-4', 'iphone-5', 'iphone-6'].forEach((phone) => {
+      it(`displays hamburger icon for phone: ${phone}`, () => {
+        cy.viewport(phone)
+        
+        cy
+        .get('#mobile-nav-toggle')
+        .should('be.visible')
+      })
+
+    })
+
+  })
+})
